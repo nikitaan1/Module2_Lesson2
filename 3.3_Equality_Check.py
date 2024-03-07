4a2337b5ceac8d324da2ebe6ac360fd0888841ec
nikitaan1 <ancevskinikita@gmail.com>
2024-03-07 09:21:52 -0500

    adding file 3.3

--- 3.3_Equality_Check.py
+++ 3.3_Equality_Check.py
@@ -8,8 +8,6 @@ if num1 == num2 and num1 == num3:
 elif num1 >= num2 and num1 >= num3:
 
 elif num2 >= num1 and num2 >= num3:
-
+    
 else:
     print(num3)
-
-
---
+++ 4.2_Century Verification.py
@@ -0,0 +1,12 @@
+year = int(input("Enter a year: "))
+leap_year = (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+century_year = year % 100 == 0
+
+if leap_year:
+    print("It is a leap year")
+
+if century_year:
+    print("It is a century year")
+
+else:
+    print("This is not a leap year")
